f69e41b0798e64d813cbfa69979ea4e3a75cdd1a
Abdussalam Abdulmuheeb <holaryhemy10@gmail.com>
2023-03-24 14:42:30 -0700

    task2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,13 @@
-# create a process named killmenow 
+# Kill a process called 'killmenow'
 
-exec { 'pkill killmenow' 
-	path    => '/bin/',
-	command => 'pkill killmenow',
+# Only kill the process if it exist
+
+# If it doesn't exists, puppet should exit
+
+# with a 0 return code and not 1
+
+
+exec {'kill `killmenow` process':
+command => '/usr/bin/pkill -9 -f killmenow',
+onlyif  => '/usr/bin/pgrep -f killmenow'
 }
