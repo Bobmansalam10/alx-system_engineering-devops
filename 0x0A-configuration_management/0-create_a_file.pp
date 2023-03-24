2bfae2b97cd6c58719b6e2478de4db729c5c37e8
Abdussalam Abdulmuheeb <holaryhemy10@gmail.com>
2023-03-24 14:34:20 -0700

    task0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create file in /tmp
+#Create a file in `/tmp/school`
 
-file { '/tmp/school' :
-	path    => '/tmp/school',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet',
-	}
+file {'/tmp/school':
+ensure  => present,
+content => 'I love Puppet',
+group   => 'www-data',
+owner   => 'www-data',
+mode    => '0744'
+}
